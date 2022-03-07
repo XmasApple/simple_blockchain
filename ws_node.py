448702605bc19ec4f0f2d634d58ffcf627c63891
XmasApple <y4blokof@yandex.ru>
2022-03-07 22:06:39 +0300

    fix redundant nodes share

--- ws_node.py
+++ ws_node.py
@@ -1,16 +1,16 @@
-from typing import List
+from typing import List, Any
 
 import asyncio
 
 import orjson
 import websockets
-from websockets.legacy.client import Connect
+from websockets import WebSocketServerProtocol
 
 from blockchain import Blockchain
 from transaction import Transaction
 
 
-async def send(websocket, message):
+async def send(websocket: WebSocketServerProtocol, message: Any):
     try:
         await websocket.send(orjson.dumps(message))
     except websockets.ConnectionClosed:
@@ -22,27 +22,35 @@ class WsNode:
         self.ip: str = ip
         self.blockchain: Blockchain = Blockchain()
         self.nodes: set[str] = set()
-        self.connects: set[websockets.WebSocketServerProtocol] = set()
+        self.connects: dict[str, WebSocketServerProtocol] = dict()
         self.mem_pool: set[Transaction] = set()
 
-    async def broadcast(self, message):
+    async def broadcast(self, message: Any):
         [asyncio.get_event_loop().create_task(send(ws, message)) for ws in self.connects]
 
     async def connect_nodes(self, nodes: List[str]):
         olds = [self.ip] + list(self.nodes)
         news = []
-        for node in [node for node in nodes if node not in self.nodes]:
+        for node in filter(lambda x: x not in olds, nodes):
             news.append(node)
             websocket = await websockets.connect(f'ws://{node}')
             await websocket.recv()
             await websocket.recv()
             self.nodes.add(node)
-            self.connects.add(websocket)
+            self.connects[node] = websocket
         inputs = [(node, olds + news) for node in news] + [(node, news) for node in olds]
         if len(news) > 1 or (len(news) > 0 and self.ip not in news):
-            await self.broadcast({'type': 'connect_nodes', 'data': {'nodes': list(self.nodes)}})
+            # await self.broadcast({'type': 'connect_nodes', 'data': {'nodes': list(self.nodes)}})
+            [asyncio.get_event_loop().create_task(self.share_nodes(*args)) for args in inputs]
+
         # with ThreadPoolExecutor(len(nodes)) as executor:
         #     executor.submit(self.get_longest_chain)
+
+    async def share_nodes(self, node: str, nodes: List[str]) -> bool:
+        print('share', nodes, 'to', node)
+        if node != self.ip:
+            ws = self.connects[node]
+            await send(ws, {'type': 'connect_nodes', 'data': {'nodes': nodes}})
 
 
 async def listen():
